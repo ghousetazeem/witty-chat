f85a8399f5ea3cd5408b11a2f0e74f6c81eadf3d
Mohd Ghouse Tazeem <74200389+ghousetazeem@users.noreply.github.com>
2023-04-15 15:40:07 +0530

    changing ./message to ./Message

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Message from './message';
+import Message from './Message';
 import SendMessage from './SendMessage';
 import { db } from '../firebase';
 import { query, collection, orderBy, onSnapshot } from 'firebase/firestore';
